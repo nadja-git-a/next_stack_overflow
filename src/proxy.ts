1df6935c134a0c5678bcdc209f37703e8280b848
nadja-git-a <nadja.smartx4@gmail.com>
2026-02-17 12:14:52 +0300

    add only auth pages redirect

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,32 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+
+const authPages = ["/authentication", "/login"];
 
 export async function proxy(request: NextRequest) {
   const cookieStore = await cookies();
   const token = cookieStore.get("token");
-  console.log("proxy", !!token);
 
-  if (!!token) {
+  const isAuthPage = authPages.includes(request.nextUrl.pathname);
+
+  if (!!token && isAuthPage) {
     return NextResponse.redirect(new URL("/home", request.url));
+  }
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/authentication", "/login"],
+  matcher: [
+    "/authentication",
+    "/login",
+    "/create",
+    "/myAccount",
+    "/mySnippets",
+  ],
 };
